e444f8038bb0f115724250052642d621eb4d832c
Othallia <vianiputriocta6@gmail.com>
2026-01-02 17:04:40 +0700

    Menambahkan HomeViewModel untuk menampilkan dan menghapus data siswa

---
+++ app/src/main/java/com/example/questfirebase_032/viewmodel/HomeViewModel.kt
@@ -0,0 +1,50 @@
+package com.example.questfirebase_032.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.questfirebase_032.model.Siswa
+import com.example.questfirebase_032.repositori.RepositorySiswa
+import kotlinx.coroutines.launch
+import java.io.IOException
+
+sealed interface HomeUiState {
+    data class Success(val siswa: List<Siswa>) : HomeUiState
+    object Error : HomeUiState
+    object Loading : HomeUiState
+}
+
+class HomeViewModel(private val repositorySiswa: RepositorySiswa) : ViewModel() {
+    var homeUiState: HomeUiState by mutableStateOf(HomeUiState.Loading)
+        private set
+
+    init {
+        getSiswa()
+    }
+
+    fun getSiswa() {
+        viewModelScope.launch {
+            homeUiState = HomeUiState.Loading
+            homeUiState = try {
+                HomeUiState.Success(repositorySiswa.getDataSiswa())
+            } catch (e: IOException) {
+                HomeUiState.Error
+            } catch (e: Exception) {
+                HomeUiState.Error
+            }
+        }
+    }
+
+    fun deleteSiswa(siswa: Siswa) {
+        viewModelScope.launch {
+            try {
+                repositorySiswa.deleteSiswa(siswa)
+                getSiswa()
+            } catch (e: Exception) {
+                homeUiState = HomeUiState.Error
+            }
+        }
+    }
+}
