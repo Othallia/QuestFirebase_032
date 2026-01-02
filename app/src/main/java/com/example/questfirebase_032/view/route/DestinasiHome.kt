b760a433f49b39da8c455bf99c1de9fd73c2e03c
Othallia <vianiputriocta6@gmail.com>
2026-01-02 16:49:35 +0700

    Menambahkan rute navigasi halaman utama (Home)

---
+++ app/src/main/java/com/example/questfirebase_032/view/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.questfirebase_032.view.route
+
+import com.example.questfirebase_032.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
