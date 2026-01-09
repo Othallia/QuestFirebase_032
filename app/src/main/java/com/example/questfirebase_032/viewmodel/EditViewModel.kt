4d388732dbd516ec059baa291ff9449d18e0bb7a
Othallia <vianiputriocta6@gmail.com>
2026-01-09 17:31:04 +0700

    Menambahkan validasi input dan update UI state pada EditViewModel

--- app/src/main/java/com/example/questfirebase_032/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/questfirebase_032/viewmodel/EditViewModel.kt
@@ -31,3 +31,17 @@ RepositorySiswa
         }
     }
 
+    fun updateUiState(detailSiswa: DetailSiswa) {
+        uiStateSiswa =
+            UIStateSiswa(
+                detailSiswa = detailSiswa, isEntryValid = validasiInput
+                    (detailSiswa)
+            )
+    }
+
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa) : Boolean {
+        return with(uiState) {
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
+
