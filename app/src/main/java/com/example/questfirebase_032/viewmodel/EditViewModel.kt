2589e9fa569c8091ca4c793cbec49967bb118e22
Othallia <vianiputriocta6@gmail.com>
2026-01-09 17:30:29 +0700

    Menambahkan EditViewModel untuk fitur edit siswa

--- app/src/main/java/com/example/questfirebase_032/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/questfirebase_032/viewmodel/EditViewModel.kt
@@ -1,2 +1,33 @@
 package com.example.questfirebase_032.viewmodel
 
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import com.example.questfirebase_032.repositori.RepositorySiswa
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.viewModelScope
+import com.example.questfirebase_032.model.DetailSiswa
+import com.example.questfirebase_032.model.UIStateSiswa
+import com.example.questfirebase_032.model.toDataSiswa
+import com.example.questfirebase_032.model.toUiStateSiswa
+import com.example.questfirebase_032.view.route.DestinasiDetail
+import kotlinx.coroutines.launch
+
+class EditViewModel(savedStateHandle: SavedStateHandle, private val repositorySiswa:
+RepositorySiswa
+) : ViewModel() {
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    private val idSiswa: String =
+        savedStateHandle.get<String>(DestinasiDetail.itemIdArg)
+            ?: error("idSiswa tidak ditemukan")
+
+    init {
+        viewModelScope.launch {
+            uiStateSiswa = repositorySiswa.getSatuSiswa(idSiswa)!!
+                .toUiStateSiswa(true)
+        }
+    }
+
