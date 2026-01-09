68f1976ad8aaf054fc3d46c5c91a76aab214c430
Othallia <vianiputriocta6@gmail.com>
2026-01-09 17:26:49 +0700

    Menambahkan DetailUiState untuk mengelola state detail siswa

--- app/src/main/java/com/example/questfirebase_032/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/questfirebase_032/viewmodel/DetailViewModel.kt
@@ -1,2 +1,19 @@
 package com.example.questfirebase_032.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.questfirebase_032.model.Siswa
+import com.example.questfirebase_032.repositori.RepositorySiswa
+import com.example.questfirebase_032.view.route.DestinasiDetail
+import kotlinx.coroutines.launch
+
+sealed interface DetailUiState {
+    data class Success(val siswa: Siswa) : DetailUiState
+    object Error : DetailUiState
+    object Loading : DetailUiState
+}
+
