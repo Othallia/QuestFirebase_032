127dbf40737949a41c61a3010a6a48eaa886a68a
Othallia <vianiputriocta6@gmail.com>
2026-01-09 17:33:36 +0700

    Menambahkan ekstensi CreationExtras untuk mengambil AplikasiDataSiswa

--- app/src/main/java/com/example/questfirebase_032/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/questfirebase_032/viewmodel/PenyediaViewModel.kt
@@ -1,21 +1,12 @@
 package com.example.questfirebase_032.viewmodel
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.questfirebase_032.repositori.AplikasiDataSiswa
 
-object PenyediaViewModel {
-    val Factory = viewModelFactory {
-        initializer {
-            HomeViewModel(aplikasiDataSiswa().container.repositorySiswa)
-        }
-        initializer {
-            EntryViewModel(aplikasiDataSiswa().container.repositorySiswa)
-        }
-    }
-}
-
 fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa =
     (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiDataSiswa)
+
