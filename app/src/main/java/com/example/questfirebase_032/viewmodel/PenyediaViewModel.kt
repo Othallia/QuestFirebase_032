2566b73d2ff081dab2681ebcd9246716186e8ece
Othallia <vianiputriocta6@gmail.com>
2026-01-09 17:34:21 +0700

    Menambahkan PenyediaViewModel untuk inisialisasi semua ViewModel

--- app/src/main/java/com/example/questfirebase_032/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/questfirebase_032/viewmodel/PenyediaViewModel.kt
@@ -10,3 +10,26 @@ import com.example.questfirebase_032.repositori.AplikasiDataSiswa
 fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa =
     (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiDataSiswa)
 
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            HomeViewModel(aplikasiDataSiswa().container.repositorySiswa)
+        }
+        initializer {
+            EntryViewModel(aplikasiDataSiswa().container.repositorySiswa)
+        }
+        initializer {
+            DetailViewModel(
+                this.createSavedStateHandle(),
+                aplikasiDataSiswa().container.repositorySiswa
+            )
+        }
+        initializer {
+            EditViewModel(
+                this.createSavedStateHandle(),
+                aplikasiDataSiswa().container.repositorySiswa
+            )
+        }
+    }
+
+}
